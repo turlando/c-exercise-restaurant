043a309a1a596b239599f363aa1b35a56f324ed2
Tancredi Orlando <tancredi.orlando@gmail.com>
2019-05-24 19:27:47 +0200

    Use more complex timetable

--- tests/test_timetable.c
+++ tests/test_timetable.c
@@ -7,7 +7,7 @@
 #define TIMETABLE_FILE "test_timetable.txt"
 
 void print_timetable(struct timetable t) {
-    for (unsigned int i = 0; i < 32; ++i) {
+    for (unsigned int i = 1; i < 32; ++i) {
         printf("Day: %02u\n", i);
         if (t._[i] == 0) {
             printf("Closed");
@@ -24,8 +24,8 @@ void print_timetable(struct timetable t) {
 
 int main(void) {
     struct timetable timetable = timetable_read_file(TIMETABLE_FILE);
-    assert(timetable._[1] == BINARY(0000,0000,0011,1000,0011,0000,0000,0000));
-    assert(timetable._[2] == BINARY(0000,0000,0011,1000,0000,0000,0000,0000));
+    assert(timetable._[1] == BINARY(0000,0000,0011,1000,0000,0000,0000,0000));
+    assert(timetable._[2] == BINARY(0000,0000,0011,1000,0111,0000,0000,0000));
     print_timetable(timetable);
     return 0;
 }
