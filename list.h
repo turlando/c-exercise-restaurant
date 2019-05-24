be31d79c9138c9fc3f317cdb53cf2152d4dc4e5f
Tancredi Orlando <tancredi.orlando@gmail.com>
2019-05-24 13:41:00 +0200

    Fix list implementation

--- list.h
+++ list.h
@@ -4,41 +4,45 @@
 #include <stddef.h>  /* Provides size_t */
 #include <stdlib.h>  /* Provides calloc */
 
-#define LIST_NAME(name) \
-    name
+#define _LIST_EL_META_FIELD __list_el_meta
 
-#define LIST_EL_NAME(name) \
-    name##_el
+#define LIST_EL_STRUCT(type)     \
+    struct _LIST_EL_META_FIELD { \
+        type *prev;              \
+        type *next;              \
+    } _LIST_EL_META_FIELD
 
-#define _NEW_LIST_EL_TYPE(name, type)    \
-    typedef struct LIST_EL_NAME(name) {  \
-        type data;                       \
-        struct LIST_EL_NAME(name) *prev; \
-        struct LIST_EL_NAME(name) *next; \
-    } LIST_EL_NAME(name)
-
-#define _NEW_LIST_TYPE(name)         \
-    typedef struct LIST_NAME(name) { \
-        size_t count;                \
-        LIST_EL_NAME(name) *head;    \
-        LIST_EL_NAME(name) *tail;    \
-    } LIST_NAME(name)
-
-#define NEW_LIST_TYPE(name, type)  \
-    _NEW_LIST_EL_TYPE(name, type); \
-    _NEW_LIST_TYPE(name)
-
-#define NEW_LIST() \
-    {              \
-       0,          \
-       NULL,       \
-       NULL        \
+#define LIST_TYPE(type) \
+    struct {            \
+        size_t count;   \
+        type *head;     \
+        type *tail;     \
     }
 
-#define NEW_LIST_EL(name, data) \
-    calloc(1, sizeof(name))
+#define NEW_LIST() \
+    {0, NULL, NULL}
 
-#define LIST_LENGHT(list) \
-      list.count;
+#define LIST_FIRST(l) \
+    (l.head)
+
+#define LIST_NEXT(e) \
+    (e->_LIST_EL_META_FIELD.next)
+
+#define LIST_PREV(e) \
+    (e->_LIST_EL_META_FIELD.prev)
+
+#define LIST_INSERT_FIRST(l, e)                      \
+    do {                                             \
+        LIST_NEXT(e) = LIST_FIRST(l);                \
+        if (LIST_FIRST(l) != NULL)                   \
+            LIST_PREV(LIST_FIRST(l)) = LIST_NEXT(e); \
+        LIST_FIRST(l) = e;                           \
+    } while(0)                                       \
+
+
+#define	LIST_FOREACH(l, e)    \
+    for ((e) = LIST_FIRST(l); \
+         (e);                 \
+         (e) = LIST_NEXT(e))
 
 #endif /* __RESTAURANT_LIST__ */
--- tests/test_list.c
+++ tests/test_list.c
@@ -1,13 +1,33 @@
 #include "../list.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-NEW_LIST_TYPE(int_list_t, int);
+struct list_el {
+    int _;
+    LIST_EL_STRUCT(struct list_el);
+};
+
+typedef LIST_TYPE(struct list_el) list_t;
 
 int main(void) {
-    LIST_NAME(int_list_t) list = NEW_LIST();
+    list_t list = NEW_LIST();
 
-    LIST_EL_NAME(int_list_t) *e1 = NEW_LIST_EL(int_list_t, 1);
-    LIST_EL_NAME(int_list_t) *e2 = NEW_LIST_EL(int_list_t, 2);
-    LIST_EL_NAME(int_list_t) *e3 = NEW_LIST_EL(int_list_t, 3);
+    struct list_el *e1 = calloc(1, sizeof(struct list_el));
+    e1->_ = 1;
+
+    struct list_el *e2 = calloc(1, sizeof(struct list_el));
+    e2->_ = 2;
+
+    LIST_INSERT_FIRST(list, e2);
+    LIST_INSERT_FIRST(list, e1);
+
+    struct list_el *cur = NULL;
+    LIST_FOREACH(list, cur) {
+        printf("%d\n", cur->_);
+    }
+
+    free(e1);
+    free(e2);
 
     return 0;
 }
