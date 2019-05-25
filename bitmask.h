8c79d8703b247a4a86e348bc3750efc84c3172ec
Tancredi Orlando <tancredi.orlando@gmail.com>
2019-05-25 20:14:19 +0200

    Add MASK_BIT

--- bitmask.h
+++ bitmask.h
@@ -1,10 +1,13 @@
 #ifndef __RESTAURANT_BITMASK__
 #define __RESTAURANT_BITMASK__
 
+#define MASK_BIT(n) \
+    (1U << n)
+
 /* Mask the bits from start (inclusive) to end (exclusive).
  * Return a 32 bit unsigned integer.
  */
-#define MASK_RANGE(start, end)\
+#define MASK_RANGE(start, end)                 \
     (((~0U) >> (32 - (end - start))) << start)
 
 #endif /* __RESTAURANT_BITMASK__ */
